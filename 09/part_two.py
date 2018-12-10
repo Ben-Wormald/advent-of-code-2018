699ea621d6b9a563dacaa769e61f5b169f677bd8
Ben-Wormald <benwormald95@gmail.com>
2018-12-10 14:18:34 +0000

    09 solve day nine

--- 09/part_two.py
+++ 09/part_two.py
@@ -1,5 +1,5 @@
 import sys
-from llist import dllist
+from pyllist import dllist
 
 with open(sys.argv[1]) as input_file:
     input_parts = list(input_file)[0].split(' ')
@@ -7,21 +7,32 @@ with open(sys.argv[1]) as input_file:
     last_marble = int(input_parts[6]) * 100
 
 circle = dllist([0])
-index, current_player = 0, 0
+current_player = 0
 scores = [0] * n_players
+current_node = circle.first
 
-for marble in range(1, last_marble):
+for marble in range(1, last_marble + 1):
     if marble % 23 == 0:
         scores[current_player] += marble
 
-        index = (index - 7) % circle.size
-        scores[current_player] += circle[index]
-        circle.remove(circle.nodeat(index))
-        index = index % circle.size
+        for i in range(7):
+            current_node = current_node.prev
+            if current_node is None:
+                current_node = circle.last
+
+        scores[current_player] += current_node.value
+
+        new_current_node = current_node.next
+        circle.remove(current_node)
+        current_node = new_current_node
 
     else:
-        index = (index + 2) % circle.size
-        circle.insert(marble, circle.nodeat(index))
+        for i in range(2):
+            current_node = current_node.next
+            if current_node is None:
+                current_node = circle.first
+
+        current_node = circle.insert(marble, current_node)
 
     current_player = (current_player + 1) % n_players
     
