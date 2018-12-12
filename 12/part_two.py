0ec374aa282b7951feb66b16f1102149085dc269
Ben Wormald <benwormald95@gmail.com>
2018-12-12 20:32:56 +0000

    12 solve day twelve

--- 12/part_two.py
+++ 12/part_two.py
@@ -3,7 +3,7 @@ import sys
 with open(sys.argv[1]) as input_file:
     input = list(input_file)
     
-state = '.' * 24 + input[0].split(' ')[2][:-1] + '.' * 24
+state = '.' * 4 + input[0].split(' ')[2][:-1] + '.' * 4
 conditions = []
 
 for line in input[2:]:
@@ -13,19 +13,33 @@ for line in input[2:]:
         'result': line_parts[2][0]
     })
 
-for generation in range(20):
+generations = 0
+stable = False
+first_index = -4
+
+while not stable:
     new_state = state
     for i_state in range(len(state) - 4):
         for condition in conditions:
             if condition['state'] == state[i_state:i_state + 5]:
                 new_state = new_state[:i_state + 2] + condition['result'] + new_state[i_state + 3:]
 
-    state = new_state
+    if new_state[1:] == state[:-1]:
+        stable = True
 
-state_id, id_sum = -24, 0
+    if new_state[-3:].count('#'):
+        new_state = new_state + '.'
+    if new_state[:3].count('#'):
+        new_state = '.' + new_state
+        first_index += -1
+
+    state = new_state
+    generations += 1
+
+state_id, id_sum = first_index, 0
 for char in state:
     if char == '#':
-        id_sum += state_id
+        id_sum += state_id + (50000000000 - generations)
     state_id += 1
 
 print(id_sum)
