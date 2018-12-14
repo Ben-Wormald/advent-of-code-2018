8030dbaeffa8ed19ec925133b32e5adb41a6891d
Ben Wormald <benwormald95@gmail.com>
2018-12-14 20:32:02 +0000

    14 solve day fourteen

--- 14/part_two.py
+++ 14/part_two.py
@@ -2,35 +2,24 @@ import sys
 
 input = sys.argv[1]
 input_digits = list(int(digit) for digit in input)
+input_length = len(input_digits)
 
 recipes = [3, 7]
 elf_one, elf_two = 0, 1
-done = False
-matched_input_digit = 0
 
-while not done:
+while True:
     recipe_one, recipe_two = recipes[elf_one], recipes[elf_two]
     new_recipe = recipe_one + recipe_two
     new_recipe_digits = list(int(digit) for digit in str(new_recipe))
 
-    recipes = recipes + new_recipe_digits
+    recipes.extend(new_recipe_digits)
 
-    for digit in new_recipe_digits:
-        print(recipes)
-        print(digit,input_digits[matched_input_digit])
-        if digit == input_digits[matched_input_digit]:
-            matched_input_digit += 1
-        elif matched_input_digit != 0:
-            matched_input_digit = 0
-            break
-
-        if matched_input_digit == len(input_digits):
-            print(len(recipes[:-len(input_digits)]))
-            done = True
-            break
+    if recipes[-input_length:] == input_digits:
+        print(len(recipes[:-input_length]))
+        break
+    elif recipes[-input_length - 1:-1] == input_digits:
+        print(len(recipes[:-input_length - 1]))
+        break
 
     elf_one = (elf_one + recipe_one + 1) % len(recipes)
     elf_two = (elf_two + recipe_two + 1) % len(recipes)
-
-    if len(recipes) > 30:
-        break
